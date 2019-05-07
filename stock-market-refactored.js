942bca8066c02885c638e3f167f15475b0524770
Britta Jorgenson <bljorgen@gmail.com>
2019-05-07 17:22:51 +0000

    Further optimized stock-market-refactored.js to only loop through the array one time.

--- stock-market-refactored.js
+++ stock-market-refactored.js
@@ -1,17 +1,13 @@
 function maxProfit(arr) {
-    let availableMin = [];
-    let currentMin = arr[0];
-
-    // Create and array of minimum purchase values available up to a point.
+    let availableMin = arr[0];
+    let availableMaxProfit = 0;
+    // Loop through array and find available minimum and available max profit for a given sell date.
     arr.forEach(value => {
-        currentMin = value < currentMin ? value : currentMin;
-        availableMin.push(currentMin);
+        availableMin = value < availableMin ? value : availableMin;
+        let currentProfit = value - availableMin;
+        availableMaxProfit = currentProfit > availableMaxProfit ? currentProfit : availableMaxProfit;
     });
-
-    // Calculate the available/possible profit that could have been made on given sell date based on possible previous purchase dates.
-    var availableProfit = arr.map((value, index) => value - availableMin[index])
-    return Math.max.apply(null, availableProfit);
+    return availableMaxProfit;
 }
-
 let Array = [45, 24, 35, 31, 40, 38, 11];
 console.log(`The max profit is: ` + maxProfit(Array));
